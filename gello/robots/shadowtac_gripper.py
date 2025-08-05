785c0af88bea01744458f0d81e57cd78acabbf48
Zico da Silva <zicods7@gmail.com>
2025-08-05 14:39:54 +0200

    Completed a basic version to open and close the shadowtac gripper from the gello arm.

--- gello/robots/shadowtac_gripper.py
+++ gello/robots/shadowtac_gripper.py
@@ -1,28 +1,64 @@
-"""Module to control Robotiq's grippers - tested with HAND-E.
-
-Taken from https://github.com/githubuser0xFFFF/py_robotiq_gripper/blob/master/src/robotiq_gripper.py
-"""
-
-import socket
+import sys
+import serial
+import struct
 import threading
 import time
-from enum import Enum
-from typing import OrderedDict, Tuple, Union
+from enum import Enum, IntEnum
+from typing import Tuple, Optional
+
+# Commands that are sent between the host and the microcontroller
+class CommandID(IntEnum):
+    CMD_GRIPPER_STATE = 0x01
+    CMD_SLIP_SAFETY_MARGIN = 0x02
+    CMD_OPEN_GRIPPER = 0x03
+    CMD_CLOSE_GRIPPER = 0x04
+    CMD_DEBUG = 0x05
+    CMD_LOG = 0x06
+
+class GripperState(IntEnum):
+    IDLE = 0x01
+    MOVING = 0x02
+    GRASPING = 0x03
+
+class CommandInterface:
+    def __init__(self, port: str, baud_rate: int, timeout_s: float = 0.001):
+        self.serial_port = serial.Serial(port, baudrate=baud_rate, timeout=timeout_s)
+    
+    def close(self):
+        self.serial_port.close()
+
+    def send_command(self, cmd: CommandID, data: bytes):
+        packet = struct.pack("<BB", cmd.value, len(data)) + data
+        self.serial_port.write(packet)
+
+    def receive_command(self) -> tuple[CommandID | None, bytes | None]:
+        header = self.serial_port.read(2)
+        if len(header) == 0:
+            return None, None  # Not enough data
+
+        cmd_id, length = struct.unpack("<BB", header)
+        data = self.serial_port.read(length)
+        if len(data) < length:
+            return None, None
+
+        try:
+            cmd = CommandID(cmd_id)
+        except ValueError:
+            cmd = cmd_id  # Unknown command
+
+        return cmd, data
 
 
 class ShadowtacGripper:
-    """Communicates with the gripper directly, via scoket connection to shadowtac"""
+    """Communicates with the gripper directly, via serial connection to shadowtac gripper"""
 
-    class ObjectStatus(Enum):
-        """Object status reported by the gripper. The integer values have to match what the gripper sends."""
-        MOVING = 0
-        STOPPED_OUTER_OBJECT = 1
-        STOPPED_INNER_OBJECT = 2
-        AT_DEST = 3
+    class State(Enum):
+        OPEN = 0x01
+        CLOSED = 0x02
 
     def __init__(self):
         """Constructor."""
-        self.socket = None
+        self.command_interface: Optional[CommandInterface] = None
         self.command_lock = threading.Lock()
         self._min_position = 0
         self._max_position = 255
@@ -30,71 +66,29 @@ class ShadowtacGripper:
         self._max_speed = 255
         self._min_force = 0
         self._max_force = 255
+        self.state = ShadowtacGripper.State.OPEN
 
-    def connect(self, hostname: str, port: int, socket_timeout: float = 10.0) -> None:
-        """Connects to a gripper at the given address.
-
-        :param hostname: Hostname or ip.
-        :param port: Port.
-        :param socket_timeout: Timeout for blocking socket operations.
-        """
-        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        assert self.socket is not None
-        self.socket.connect((hostname, port))
-        self.socket.settimeout(socket_timeout)
+    def connect(self, port: str, baud_rate: int, timeout_s: float = 0.001) -> None:
+        """Connects to the gripper over serial"""
+        try:
+            self.command_interface = CommandInterface(port, baud_rate=baud_rate, timeout_s=timeout_s)
+            # Ensure that we are in a known state at start.
+            self.command_interface.send_command(CommandID.CMD_OPEN_GRIPPER, struct.pack(""))
+        except serial.SerialException as e:
+            sys.exit(e) # type: ignore
 
     def disconnect(self) -> None:
         """Closes the connection with the gripper."""
-        assert self.socket is not None
-        self.socket.close()
+        assert self.command_interface is not None
+        self.command_interface.close()
 
     def _reset(self):
         # TODO: add reset.
         pass
 
-    def activate(self, auto_calibrate: bool = True):
-        """Resets the activation flag in the gripper, and sets it back to one, clearing previous fault flags.
-
-        :param auto_calibrate: Whether to calibrate the minimum and maximum positions based on actual motion.
-
-        The following code is executed in the corresponding script function
-        def rq_activate(gripper_socket="1"):
-            if (not rq_is_gripper_activated(gripper_socket)):
-                rq_reset(gripper_socket)
-
-                while(not rq_get_var("ACT", 1, gripper_socket) == 0 or not rq_get_var("STA", 1, gripper_socket) == 0):
-                    rq_reset(gripper_socket)
-                    sync()
-                end
-
-                rq_set_var("ACT",1, gripper_socket)
-            end
-        end
-
-        def rq_activate_and_wait(gripper_socket="1"):
-            if (not rq_is_gripper_activated(gripper_socket)):
-                rq_activate(gripper_socket)
-                sleep(1.0)
-
-                while(not rq_get_var("ACT", 1, gripper_socket) == 1 or not rq_get_var("STA", 1, gripper_socket) == 3):
-                    sleep(0.1)
-                end
-
-                sleep(0.5)
-            end
-        end
-        """
-        if not self.is_active():
-            # TODO:
-            pass
-
-        # auto-calibrate position range if desired
-        if auto_calibrate:
-            self.auto_calibrate()
-
     def is_active(self):
         """Returns whether the gripper is active."""
-        return False
+        return True
 
     def get_min_position(self) -> int:
         """Returns the minimum position the gripper can reach (open position)."""
@@ -122,15 +116,7 @@ class ShadowtacGripper:
 
     def get_current_position(self) -> int:
         """Returns the current position as returned by the physical hardware."""
-        return 0
-
-    def auto_calibrate(self, log: bool = True) -> None:
-        # TODO:
-
-        if log:
-            print(
-                f"Gripper auto-calibrated to [{self.get_min_position()}, {self.get_max_position()}]"
-            )
+        return 0 if self.state == ShadowtacGripper.State.OPEN else 255
 
     def move(self, position: int, speed: int, force: int) -> Tuple[bool, int]:
         """Sends commands to start moving towards the given position, with the specified speed and force.
@@ -152,51 +138,32 @@ class ShadowtacGripper:
         clip_spe = clip_val(self._min_speed, speed, self._max_speed)
         clip_for = clip_val(self._min_force, force, self._max_force)
 
-        # TODO: moves to the given position with the given speed and force
+        success = False
+        if self.command_interface is not None:
+            if clip_pos == self._min_position:
+                self.command_interface.send_command(CommandID.CMD_OPEN_GRIPPER, struct.pack(""))
+                self.state = ShadowtacGripper.State.OPEN
+                success = True
+            elif clip_pos == self._max_position:
+                self.command_interface.send_command(CommandID.CMD_CLOSE_GRIPPER, struct.pack(""))
+                self.state = ShadowtacGripper.State.CLOSED
+                success = True
         
-        return False, clip_pos
-
-    def move_and_wait_for_pos(
-        self, position: int, speed: int, force: int
-    ) -> Tuple[int, ObjectStatus]:  # noqa
-        """Sends commands to start moving towards the given position, with the specified speed and force, and then waits for the move to complete.
-
-        :param position: Position to move to [min_position, max_position]
-        :param speed: Speed to move at [min_speed, max_speed]
-        :param force: Force to use [min_force, max_force]
-        :return: A tuple with an integer representing the last position returned by the gripper after it notified
-        that the move had completed, a status indicating how the move ended (see ObjectStatus enum for details). Note
-        that it is possible that the position was not reached, if an object was detected during motion.
-        """
-        position = int(position)
-        speed = int(speed)
-        force = int(force)
-
-        set_ok, cmd_pos = self.move(position, speed, force)
-        if not set_ok:
-            raise RuntimeError("Failed to set variables for move.")
-
-        # TODO: wait until the gripper acknowledges that it will try to go to the requested position
-
-        # TODO: wait until not moving
-
-        # TODO: report the actual position and the object status
-        return (0, ShadowtacGripper.ObjectStatus.AT_DEST)
+        return success, clip_pos
 
 
 def main():
     # test open and closing the gripper
     gripper = ShadowtacGripper()
-    gripper.connect(hostname="192.168.1.10", port=63352)
-    # gripper.activate()
+    gripper.connect(port="/dev/ttyACM0", baud_rate=115200)
     print(gripper.get_current_position())
-    gripper.move(20, 255, 1)
+    gripper.move(255, 255, 1)
     time.sleep(0.2)
     print(gripper.get_current_position())
-    gripper.move(65, 255, 1)
+    gripper.move(0, 255, 1)
     time.sleep(0.2)
     print(gripper.get_current_position())
-    gripper.move(20, 255, 1)
+    gripper.move(255, 255, 1)
     gripper.disconnect()
 
 
